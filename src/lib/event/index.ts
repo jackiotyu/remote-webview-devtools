42877a8ade9e82c6ca94f05276d1662b0459b8cb
jackiotyu <2504448153@qq.com>
2023-05-08 00:03:40 +0800

    fix: 事件总栈
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/lib/event/index.ts
+++ src/lib/event/index.ts
@@ -2,12 +2,14 @@ import * as vscode from 'vscode';
 
 import AdbEvent from './adbEvent';
 import EditorEvent from './editorEvent';
+import TunnelEvent from './tunnelEvent';
 
 export default class EventDisposables {
     constructor(extension: vscode.ExtensionContext) {
         extension.subscriptions.push(
             ...AdbEvent.getDisposables(),
             ...EditorEvent.getDisposables(),
+            ...TunnelEvent.getDisposables(),
         )
     }
 }
